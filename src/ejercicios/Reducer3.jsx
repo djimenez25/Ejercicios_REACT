27eb2cdd4757ba13adc527f5a5a5a4b90c433633
djimenez25 <d.jimenez25@hotmail.com>
2025-02-26 11:36:59 +0100

    Ejercicio reducer 3 actualizado

--- src/ejercicios/Reducer3.jsx
+++ src/ejercicios/Reducer3.jsx
@@ -1,4 +1,4 @@
-import { act, useReducer } from "react";
+import { act, useReducer, useRef } from "react";
 
 const formReducer = (state, action) => {
   switch (action.type) {
@@ -18,36 +18,44 @@ const formReducer = (state, action) => {
 export function Reducer3() {
   const [state, dispatch] = useReducer(formReducer, []);
 
+  const enviarFormulario = (e) => {
+    e.preventDefault();
+    console.log("Datos enviados:", JSON.stringify(state));
+  };
   return (
     <>
       <button onClick={() => dispatch({ type: "AGREGAR_CAMPO" })}>
         Agregar campo
       </button>
 
-      <div>
-        {state.map((campo) => (
-          <div key={campo.id}>
-            <input
-              type='text'
-              value={campo.value}
-              onChange={(e) =>
-                dispatch({
-                  type: "ACTUALIZAR_VALOR",
-                  idCampo: campo.id,
-                  nuevoCampo: e.target.value,
-                })
-              }
-            />
-            <button
-              onClick={() =>
-                dispatch({ type: "ELIMINAR_CAMPO", idCampo: campo.id })
-              }
-            >
-              Eliminar
-            </button>
-          </div>
-        ))}
-      </div>
+      <form onSubmit={enviarFormulario}>
+        <div>
+          {state.map((campo) => (
+            <div key={campo.id}>
+              <input
+                type='text'
+                value={campo.value}
+                onChange={(e) =>
+                  dispatch({
+                    type: "ACTUALIZAR_VALOR",
+                    idCampo: campo.id,
+                    nuevoCampo: e.target.value,
+                  })
+                }
+              />
+              <button
+                onClick={() =>
+                  dispatch({ type: "ELIMINAR_CAMPO", idCampo: campo.id })
+                }
+              >
+                Eliminar
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <button type='submit'>Enviar</button>
+      </form>
     </>
   );
 }
