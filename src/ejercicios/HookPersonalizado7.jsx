ea0eaa7af20cdb8dbe613f920a40edfd691b2eb0
djimenez25 <d.jimenez25@hotmail.com>
2025-03-07 19:01:32 +0100

    Commit hookPersonalizados

--- src/ejercicios/HookPersonalizado7.jsx
+++ src/ejercicios/HookPersonalizado7.jsx
@@ -1,7 +1,19 @@
+import { useArray } from "../hooks/useArray";
+
 export function HookPersonalizado7() {
+  const { array, addItem, removeItem, updateItem } = useArray([1, 2, 3]);
   return (
     <>
-      <h1>Hola</h1>
+      <ul>
+        {array.map((e, index) => (
+          <li key={index}>
+            {e}
+            <button onClick={() => removeItem(index)}>Eliminar</button>
+            <button onClick={() => updateItem(index, e * 10)}>Editar</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => addItem(array.length + 1)}>Añadir elemento</button>
     </>
   );
 }
--- src/hooks/useArray.jsx
+++ src/hooks/useArray.jsx
@@ -1,1 +1,19 @@
-export function useArray() {}
+import { useState } from "react";
+
+export function useArray(estadoInicial = []) {
+  const [array, setArray] = useState(estadoInicial);
+
+  const addItem = (item) => {
+    setArray([...array, item]);
+  };
+
+  const removeItem = (index) => {
+    setArray(array.filter((_, indice) => indice !== index));
+  };
+
+  const updateItem = (index, newItem) => {
+    setArray(array.map((e, indice) => (indice === index ? newItem : e)));
+  };
+
+  return { array, addItem, removeItem, updateItem };
+}
